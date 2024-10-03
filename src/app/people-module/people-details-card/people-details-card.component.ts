6c2ffa92de6f130dc7ffdb64bf42c90c86e4074a
M1keTrike <miguemolina4570@gmail.com>
2024-10-02 18:48:42 -0600

    [ADD] added components of people module

---
+++ src/app/people-module/people-details-card/people-details-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-details-card',
+  templateUrl: './people-details-card.component.html',
+  styleUrl: './people-details-card.component.css'
+})
+export class PeopleDetailsCardComponent {
+
+}
---
+++ src/app/people-module/people-search-bar/people-search-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-search-bar',
+  templateUrl: './people-search-bar.component.html',
+  styleUrl: './people-search-bar.component.css'
+})
+export class PeopleSearchBarComponent {
+
+}
